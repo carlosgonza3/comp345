370cb7138c514901bfab6f6c9d5636054b1796ca
Tony Phan <tonyphanmc@hotmail.com>
2024-09-26 00:11:44 -0400

    finish card class, start deck and hand

--- Cards.cpp
+++ Cards.cpp
@@ -19,6 +19,14 @@ Card::Card(const Card& otherCard){
     cardType = new string(*otherCard.cardType);
 }
 
+Card& Card::operator=(const Card& other) {
+    if (this != &other) {
+        delete cardType; 
+        cardType = new std::string(*(other.cardType)); 
+    }
+    return *this; 
+}
+
 Card::~Card(){
     delete cardType;
 }
@@ -27,19 +35,33 @@ std::ostream& operator<<(std::ostream& os, const Card& card) {
     os << "Card Type: " << *(card.cardType); 
     return os; 
 }
-
 void Card::play(){
-    
-    //add order to player's list of orders
+    //create and add an order to player's list of orders
     //Remove card from hand and put it back into the player's hand
 }
-
-
-
 std::string Card::getCardType(){
     return *cardType;
 }
 
+
+Deck::Deck(){
+    deck_cards.push_back(new Card("Bomb"));
+    deck_cards.push_back(new Card("Reinforcement"));
+    deck_cards.push_back(new Card("Blockade"));
+    deck_cards.push_back(new Card("Airlift"));
+    deck_cards.push_back(new Card("Diplomacy"));
+}
+
+Card* Deck::draw() {
+    if (deck_cards.empty()) {
+        return nullptr; // No cards left to draw
+    }
+    
+    // Generate a random index to draw a card and return it directly
+    Card* drawnCard = deck_cards.back(); // Get the last card as a ptr
+    deck_cards.pop_back(); // Remove the last card from the deck
+    return drawnCard; // Return the drawn card
+}
 
 
 int main(){
--- Cards.h
+++ Cards.h
@@ -7,13 +7,11 @@
 
 class Card {
 public:
-    Card(const std::string& cardType); //Constructor using pass by reference
+    Card(const std::string& cardType);  //Constructor using pass by reference
     Card(const Card& other);            //Copy constructor using pass by reference
     Card& operator=(const Card& other); // Assignment operator
-    ~Card();
-
+    ~Card();                            //Destructor
     friend std::ostream& operator<<(std::ostream& os, const Card& card); //Stream insert. op
-
     void play(); //Also takes in a pointer to a deck object so it appends to the deck
     std::string getCardType();          //Getter
     
@@ -26,12 +24,15 @@ private:
 
 class Deck {
 public:
-    Deck(); //Constructor to build the deck/cards
-
+    Deck();                             //Constructor to build the deck/cards
+    Deck(const Deck& copyDeck);         //Copy const
+    Deck& operator=(const Deck& other); //Assignment op
+    ~Deck();                            //Deconst.
+    friend std::ostream& operator<<(std::ostream& os, const Deck& deck); //Stream insert. op
     Card* draw(); //Draw method that hand card will call to get cards
 
 private:
-    std::vector<Card*> cards;
+    std::vector<Card*> deck_cards;
 
 };
 
@@ -41,10 +42,10 @@ private:
 class Hand {
 public:
     Hand();
-
-
+    Hand(const Hand& copyHand);
+    Hand& operator=(const Hand& other);
 private:
-    std::vector<Card*> cards; //ArrayList of cards 
+    std::vector<Card*> hand_cards; //ArrayList of cards 
 };
 
 
