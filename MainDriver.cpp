160162798a6d0f297addfbc6f3ddc530148ef951
Tony Phan <tonyphanmc@hotmail.com>
2024-11-10 04:20:09 -0500

    final changes

--- MainDriver.cpp
+++ MainDriver.cpp
@@ -15,39 +15,33 @@
 
 int main() {
 
-<<<<<<< HEAD
     //testStartupPhase();
 
     //LogObserver log_observer;
-=======
     //LogObserver logObserver;
 
     // Part 1
     //testCommandProcessor();
->>>>>>> 8dc8e66b94bc6f034d45f4cdfb5570b5d400d03f
 
     // Part 2
     //testStartupPhase();
 
-<<<<<<< HEAD
     //testLoggingObserver();
 
-    testMainGameLoop();
+    
 
 
    //testCommandProcessor();
 
-   //testOrderExecution();
-=======
+   
     // Part 3
-    // missing...
+    // testMainGameLoop();
 
     // Part 4
     //testOrderExecution();
 
     // Part 5
     //testLoggingObserver();
->>>>>>> 8dc8e66b94bc6f034d45f4cdfb5570b5d400d03f
 
     
     return 0;
